63435a388d59f7f06d4eb87a914dc2768008015f
Prince527 <75485262+Prince527GitHub@users.noreply.github.com>
2022-08-15 08:59:57 -0300

    updated

--- web/ssmp-bot/js/script.js
+++ web/ssmp-bot/js/script.js
@@ -9,7 +9,7 @@ window.onload = () => setTimeout(() => {
     body.classList.remove("disabled")
 }, 1550);
 
-var isMobile = {
+const isMobile = {
     Android: function () {
         return navigator.userAgent.match(/Android/i);
     },
@@ -26,9 +26,16 @@ var isMobile = {
         return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
     },
     any: function () {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile
-            .Windows());
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
     }
 };
 
-if (isMobile.any()) window.location.href = 'https://serversmp-api.herokuapp.com/bot';
+if (isMobile.any()) {
+    const link = document.createElement('link')
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '/web/ssmp-bot/css/style.mobile.css';
+    document.getElementsByTagName('HEAD')[0].appendChild(link);
+    document.getElementById("mobile-grid-1").innerHTML = `<div class="box"><img src="./img/icon.png" width="70%" height="70%"></div><div class="box"><h1 class="box-title">ServerSMP - BOT</h1><p class="box-description">A bot for all your needs! May it be moderation, info on things from your server, or even random pictures of ducks. We have it all!</p></div>`;
+    document.getElementById("mobile-grid-2").innerHTML = `<div class="box"><img src="./img/search.png" width="90%" height="90%"></div><div class="box"><h1 class="box-title">Search</h1><p class="box-description">We have a selections of commands to search websites!</p></div>`;
+}
