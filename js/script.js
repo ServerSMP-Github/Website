7b3b333e0a1d8c0cf31a3db61dfb85fd6524d3d6
Prince527 <Prince527gaming@gmail.com>
2025-04-24 19:44:56 -0300

    Start "add animation"

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const loading = document.getElementById("loading");
-const body = document.getElementById("body");
-
-window.onload = () => setTimeout(() => {
-    loading.classList.add("disabled");
-    body.classList.remove("disabled")
-}, 1550);
