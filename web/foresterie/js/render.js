c483b3ceedf64767a4dfaf6226c8c36fc7ac0ee2
Prince527 <prince527@duck.com>
2026-02-11 21:15:57 -0400

    Cleanup

--- web/foresterie/js/render.js
+++
@@ -1,9 +0,0 @@
-const converter = new showdown.Converter({
-    extensions: [
-        'anchor',
-    ]
-});
-
-converter.setFlavor('github');
-
-document.getElementById("subContainer").innerHTML = converter.makeHtml(document.getElementById("content").value);
